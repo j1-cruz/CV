ed897f5a49a1938b91c170613b534a7e61fa5aaf
Juan Cruz <membrillajc@gmail.com>
2019-02-11 22:04:09 -0300

    initial commit

---
+++ src/resumeData.js
@@ -0,0 +1,118 @@
+let resumeData = {
+  "imagebaseurl":"https://techonoeticsclub.github.io/",
+  "name": "Juan Cruz",
+  "role": "Frontend Developer",
+  "linkedinId":"Your LinkedIn Id",
+  "skypeid": "Your skypeid",
+  "roleDescription": " (en foramación). Me gusta incursionar en varias partes del desarrollo frontend y me gusta aprender sobre nuevas tecnologías. Enfoncado (en un principio) en la utilización de JS",
+  "socialLinks":[
+      {
+        "name":"linkedin",
+        "url":"https://www.linkedin.com/",
+        "className":"fa fa-linkedin"
+      },
+      {
+        "name":"github",
+        "url":"http://github.com/",
+        "className":"fa fa-github"
+      },
+      {
+        "name":"skype",
+        "url":"http://skype.com",
+        "className":"fa fa-skype"
+      }
+    ],
+  "aboutme":"Autodidacta. Estoy aprendiendo desarrollo web full stack utlizando java script",
+  "address":"Argentina",
+  "website":"https://www.codeclassifiers.com",
+  "education":[
+    {
+      "UniversityName":"Mumbai University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "UniversityName":"Some University",
+      "specialization":"Some specialization",
+      "MonthOfPassing":"Jan",
+      "YearOfPassing":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "work":[
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    },
+    {
+      "CompanyName":"Some Company",
+      "specialization":"Some specialization",
+      "MonthOfLeaving":"Jan",
+      "YearOfLeaving":"2018",
+      "Achievements":"Some Achievements"
+    }
+  ],
+  "skillsDescription":"Your skills here",
+  "skills":[
+    {
+      "skillname":"HTML5"
+    },
+    {
+      "skillname":"CSS"
+    },
+    {
+      "skillname":"Reactjs"
+    },
+    {
+      "skillname":"Bootstrap"
+    },
+    {
+      "skillname":"Nodejs"
+    },
+    {
+      "skillname":"Express"
+    },
+    {
+      "skillname":"Git"
+    }
+  ],
+  "portfolio":[
+    {
+      "name":"project1",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    },
+    {
+      "name":"project2",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project.jpg"
+    },
+    {
+      "name":"project3",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/project2.png"
+    },
+    {
+      "name":"project4",
+      "description":"mobileapp",
+      "imgurl":"images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials":[
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    },
+    {
+      "description":"This is a sample testimonial",
+      "name":"Some technical guy"
+    }
+  ]
+}
+
+export default resumeData
